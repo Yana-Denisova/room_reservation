6378dcf2600dee38cdb7357241c56d1c54217423
Yana-Denisova <ladymacbeth@list.ru>
2022-12-27 18:23:34 +0300

    added check_reservation_intersections validator

--- apps/crud/reservation.py
+++ apps/crud/reservation.py
@@ -1,5 +1,19 @@
-# app/crud/reservation.py
+from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from crud.base import CRUDBase
 from models.reservation import Reservation
 
-reservation_crud = CRUDBase(Reservation)
+
+class CRUDReservation(CRUDBase):
+    
+    async def get_reservations_at_the_same_time(
+        self,
+        from_reserve: datetime,
+        to_reserve: datetime,
+        meetingroom_id: int,
+        session: AsyncSession,
+    ) -> list[Reservation]:
+        return []
+
+reservation_crud = CRUDReservation(Reservation)
