a39882e8c259b35d16cbca83f7f4ed1e5a156006
Yana-Denisova <ladymacbeth@list.ru>
2023-01-04 18:04:39 +0300

    change delete users

--- apps/core/config.py
+++ apps/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_description: str = 'Перспективный сервис для бизнеса'
     database_url: str = 'sqlite+aiosqlite:///../fastapi.db'
+    secret: str = 'secret'
 
     class Config:
         env_file = '.env'
